9e37b1e97490b543b8b8904f611ff5196be7fcaf
S-Yamadaa <120069061+S-Yamadaa@users.noreply.github.com>
2022-12-15 14:13:28 +0900

    あああ

---
+++ web.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='./templates/images')
+
+@app.route('/')
+def index():
+    return render_template('index.html') #htmlファイルの表示
+
+if __name__ == '__main__':
+    app.run()
