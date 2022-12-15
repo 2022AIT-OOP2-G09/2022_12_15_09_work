6ccfbca0a3ebdca11aee7ec0171a8a6c8d05c44c
RyoHatano <ryohatano0809@gmail.com>
2022-12-15 15:57:07 +0900

    グレースケール変換の処理を実装

--- image_processing.py
+++ image_processing.py
@@ -1,2 +1,18 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import cv2
+import os
+
+class GrayScale():
+    def __init__(self):
+        self.path = os.listdir('./input_image')
+        self.threshold = 100
+        
+    def convert(self):
+        for each_path in self.path:
+            img = cv2.imread(f'input_image/{each_path}', 0)
+            print(img)
+            ret, img_thresh = cv2.threshold(img, self.threshold, 255, cv2.THRESH_BINARY)
+            cv2.imwrite(f'output_grayscale_image/{each_path}', img_thresh)
+        
+if __name__ == "__main__":
+    app = GrayScale()
+    app.convert()
