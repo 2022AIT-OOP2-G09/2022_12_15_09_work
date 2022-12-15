b6e1d94ba857c4cc6f84f1c2bbe78f6e5d8e2d0d
iu-A <imaizumi2002@gmail.com>
2022-12-15 15:54:28 +0900

    Cannyの関数化

--- image_processing.py
+++ image_processing.py
@@ -4,12 +4,12 @@ import cv2
 import numpy as np
 import os
 
-class Cannyclass():
-    def Cannydef():
-        path = './input_image'
-        inputlist = os.listdir(path)
 
-        for i in inputlist:
-            img = cv2.imread(f'./input_image/{i}',0)
-            edges = cv2.Canny(img,100,200)
-            cv2.imwrite(f'./output_cannyfilter_image/{i}',edges)
+def Cannydef():
+    path = './input_image'
+    inputlist = os.listdir(path)
+
+    for i in inputlist:
+        img = cv2.imread(f'./input_image/{i}',0)
+        edges = cv2.Canny(img,100,200)
+        cv2.imwrite(f'./output_cannyfilter_image/{i}',edges)
