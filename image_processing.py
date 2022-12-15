f1fb419322c6404d35c256f4d8ece9cad2cf1dee
iu-A <imaizumi2002@gmail.com>
2022-12-15 15:26:25 +0900

    cannyの実装

--- image_processing.py
+++ image_processing.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import cv2
+import numpy as np
+import os
+
+path = './input_image'
+inputlist = os.listdir(path)
+
+for i in inputlist:
+    img = cv2.imread(inputlist[i],0)
+    edges = cv2.Canny(img,100,200)
+    cv2.imwrite('/output_cannyfilter_image/{inputlist[i]}',edges)
