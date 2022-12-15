16470cc215e1a43f8e9551a8d5458a1cfa0fc6c5
iu-A <imaizumi2002@gmail.com>
2022-12-15 15:48:42 +0900

    cannyのクラス化と間違いの変更

--- image_processing.py
+++ image_processing.py
@@ -4,10 +4,12 @@ import cv2
 import numpy as np
 import os
 
-path = './input_image'
-inputlist = os.listdir(path)
+class Cannyclass():
+    def Cannydef():
+        path = './input_image'
+        inputlist = os.listdir(path)
 
-for i in inputlist:
-    img = cv2.imread(inputlist[i],0)
-    edges = cv2.Canny(img,100,200)
-    cv2.imwrite('/output_cannyfilter_image/{inputlist[i]}',edges)
+        for i in inputlist:
+            img = cv2.imread(f'./input_image/{i}',0)
+            edges = cv2.Canny(img,100,200)
+            cv2.imwrite(f'./output_cannyfilter_image/{i}',edges)
