eddb5f955a2f1aa41f49d537bd71da305fbd82ba
RyoHatano <ryohatano0809@gmail.com>
2022-12-19 13:34:29 +0900

    delete: print(img)

--- image_processing.py
+++ image_processing.py
@@ -9,7 +9,6 @@ class GrayScale():
     def convert(self):
         for each_path in self.path:
             img = cv2.imread(f'input_image/{each_path}', 0)
-            print(img)
             ret, img_thresh = cv2.threshold(img, self.threshold, 255, cv2.THRESH_BINARY)
             cv2.imwrite(f'output_grayscale_image/{each_path}', img_thresh)
         
