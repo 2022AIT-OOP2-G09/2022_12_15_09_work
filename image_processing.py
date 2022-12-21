e89f8fedb54e888bb694980c7393a1a8e4c6a635
esukarugon <dededeoroshi0108@gmail.com>
2022-12-21 16:56:20 +0900

    web と　canny grayとの対応関係はできた

--- image_processing.py
+++ image_processing.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from werkzeug.utils import secure_filename
+from watchdog.events import FileSystemEventHandler
 import cv2
 import numpy as np
 import os
@@ -7,25 +9,34 @@ import os
 
 class Cannyclass():
     def __init__(self):
-        self.path = os.listdir('./input_image')
+        self.path = os.listdir('./static/input_image')
 
     def convert_canny(self):
         for each_path in self.path:
-            img = cv2.imread(f'./input_image/{each_path}',0)
+            img = cv2.imread(f'./static/input_image/{each_path}',0)
             edges = cv2.Canny(img,100,200)
-            cv2.imwrite(f'./output_cannyfilter_image/{each_path}',edges)
+            cv2.imwrite(f'./static/output_cannyfilter_image/{each_path}',edges)
 
 class GrayScale():
     def __init__(self):
-        self.path = os.listdir('./input_image')
+        self.path = os.listdir('./static/input_image')
         self.threshold = 100
         
     def convert(self):
         for each_path in self.path:
-            img = cv2.imread(f'input_image/{each_path}', 0)
+            img = cv2.imread(f'./static/input_image/{each_path}', 0)
             print(img)
             ret, img_thresh = cv2.threshold(img, self.threshold, 255, cv2.THRESH_BINARY)
-            cv2.imwrite(f'output_grayscale_image/{each_path}', img_thresh)
+            cv2.imwrite(f'./static/output_grayscale_image/{each_path}', img_thresh)
+
+class ChangeHandler(FileSystemEventHandler):
+    # すべてのイベント
+    def on_any_event(self, event):
+        app = GrayScale()
+        app.convert()
+        app = Cannyclass()
+        app.convert_canny()
+        print("aaaaaaa")
 
 if __name__ == "__main__":
     app = GrayScale()
--- web.py
+++ web.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request, url_for, redirect
 from werkzeug.utils import secure_filename
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
+import time
+import image_processing
 import os
 
 app = Flask(__name__)
@@ -13,7 +18,7 @@ def index():
 #
 @app.route("/input_image" , methods=["GET"])
 def Input_IMG():
-    IMG_LIST = os.listdir('static/input_image')
+    IMG_LIST = os.listdir('./static/input_image')
     IMG_LIST = ['input_image/' + i for i in IMG_LIST]
     imagelist=IMG_LIST
     return render_template("input_image.html", imagelist = imagelist)
@@ -37,7 +42,7 @@ def Mosaic_IMG():
 #
 @app.route("/gray_image" , methods=["GET"])
 def Glay_IMG():
-    IMG_LIST = os.listdir('output_grayscale_image')
+    IMG_LIST = os.listdir('static/output_grayscale_image')
     IMG_LIST = ['output_grayscale_image/' + i for i in IMG_LIST]
     imagelist=IMG_LIST
     return render_template("gray_image.html", imagelist = imagelist)
@@ -45,12 +50,11 @@ def Glay_IMG():
 #
 @app.route("/canny_image" , methods=["GET"])
 def Canny_IMG():
-    IMG_LIST = os.listdir('output_cannyfilter_image')
+    IMG_LIST = os.listdir('static/output_cannyfilter_image')
     IMG_LIST = ['output_cannyfilter_image/' + i for i in IMG_LIST]
     imagelist=IMG_LIST
     return render_template("canny_image.html", imagelist = imagelist)
     
-#
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     # URLでhttp://127.0.0.1:5000/uploadを指定したときはGETリクエストとなるのでこっち
@@ -59,7 +63,7 @@ def upload():
     # formでsubmitボタンが押されるとPOSTリクエストとなるのでこっち
     elif request.method == 'POST':
         file = request.files['example']
-        file.save(os.path.join('./input_image', file.filename))
+        file.save(os.path.join('./static/input_image', file.filename))
         return redirect(url_for('uploaded_file', filename=file.filename))
 
 
@@ -67,6 +71,15 @@ def upload():
 def uploaded_file(filename):
     return render_template('uploaded_file.html', filename=filename)
 
-if __name__=='__main__':
-    app.run(debug=True)
 
+
+if __name__ == "__main__":
+
+    observer = Observer()
+    # 監視するフォルダを第２引数に指定
+    observer.schedule(image_processing.ChangeHandler(), './static/input_image', recursive=True)
+    # 監視を開始する
+    observer.start()
+    while True:
+        time.sleep(5)
+        app.run(debug=True)
